9b6bfcc07a8ed167da83348c4436bec29447a5cd
Iñigo Quintana <quintanadinigo@gmail.com>
2024-12-02 03:08:40 -0600

    new port

--- api.py
+++ api.py
@@ -96,4 +96,5 @@ def upload_info():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 4000))
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
